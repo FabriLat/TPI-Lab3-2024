023bae82335819d5fc2a7a710baa59f74e0b2cb5
Priscila Di Renzo <prisciladirenzo@gmail.com>
2024-06-23 20:26:30 -0300

    corrección ThemeContext para persistencia de seteo

--- CinemaProyect/src/components/services/theme/theme.context.jsx
+++ CinemaProyect/src/components/services/theme/theme.context.jsx
@@ -1,30 +1,29 @@
-import { useState, createContext } from "react";
+import { useState, createContext, useEffect } from "react";
 
 export const ThemeContext = createContext();
 
-export const ThemeContextProvider = ({children}) =>{
-    
-    const [theme, setTheme] = useState("light");
+export const ThemeContextProvider = ({ children }) => {
 
-    const toggleTheme = () =>{
+  const storedTheme = localStorage.getItem("theme") || "light";
+  const [theme, setTheme] = useState(storedTheme);
 
-        if(theme ==="light"){
-            document.body.style.backgroundColor = 'black'
-            localStorage.setItem("theme", "dark");
-            setTheme("black");
-        }
-        else{
-            document.body.style.backgroundColor = '#817a9e';
-            localStorage.setItem("theme", "light");
-            setTheme("light");
-        }
-    };
+  const toggleTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+    localStorage.setItem("theme", newTheme);
+  };
 
-    return(
-        <ThemeContext.Provider value={{theme, toggleTheme}}>
-            {children}
-        </ThemeContext.Provider>
-    )
+  useEffect(() => {
+    if (theme === "dark") {
+      document.body.style.backgroundColor = "black";
+    } else {
+      document.body.style.backgroundColor = "#817a9e";
+    }
+  }, [theme]);
 
-
-}
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
