e46118c1b53f64a68853110ee7559f89c3a1f22b
Priscila Di Renzo <prisciladirenzo@gmail.com>
2024-06-23 19:57:01 -0300

    traslado de fetcheo de movies a componente App

--- CinemaProyect/src/App.jsx
+++ CinemaProyect/src/App.jsx
@@ -7,9 +7,27 @@ import MoviesDashboard from "./components/moviesDashboard/MoviesDashboard";
 import Protected from "./routes/Protected";
 import NotFound from "./routes/NotFound";
 import UserBase from "./components/userBase/UserBase";
-
+import { useEffect } from "react";
 
 function App() {
+
+  const [movies, setMovies] = useState([]);
+
+  //------------------------------------------------------------------------------
+  // FETCHEO A MOVIES
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const response = await fetch("http://localhost:8000/movies");
+        const movieData = await response.json();
+        setMovies(movieData);
+      } catch (error) {
+        console.log("Error al solicitar películas a la base de datos:", error);
+      }
+    };
+    fetchMovies();
+  }, []); 
+  // -----------------------------------------------------------------------------------------------------------------------------------------------------------
 
   // estado para manejar log in
   const [isLoggedIn, setIsLoggedIn] = useState(false); // inicialmente no se logueó el usuario
@@ -112,123 +130,24 @@ function App() {
 
     setListUser(listUpdated);
   };
-  //--------------------------------------------------------------------------------------------------------------------------------------
-
-  // //Una pelicula puede tener varias funciones.
-  // const initialMovies = [
-  //   {
-  //     id: 1,
-  //     title: "Shrek",
-  //     image: "https://es.web.img3.acsta.net/pictures/14/03/06/10/13/369709.jpg",
-  //     rating: 5.0,
-  //     runTime: 89,
-  //     shows: [{}, {}, {}],
-  //   },
-  //   {
-  //     id: 2,
-  //     title: "Oppenheimer",
-  //     image:
-  //       "https://www.rosariocine.com.ar/webfiles/rosariocine/productos/5132/1.jpg",
-  //     rating: 5.0,
-  //     runTime: 180,
-  //     shows: [{}, {}, {}],
-  //   },
-  //   {
-  //     id: 3,
-  //     title: "Hostel",
-  //     image:
-  //       "https://m.media-amazon.com/images/M/MV5BMTY1NDA3ODM1OV5BMl5BanBnXkFtZTcwMDM5NzEzMQ@@._V1_.jpg",
-  //     rating: 4.0,
-  //     runTime: 94,
-  //     shows: [{}, {}, {}],
-  //   },
-  //   {
-  //     id: 4,
-  //     title: "Saw",
-  //     image:
-  //       "https://m.media-amazon.com/images/I/61Dr1uVEjWL._AC_UF894,1000_QL80_.jpg",
-  //     rating: 4.0,
-  //     runTime: 103,
-  //     shows: [{}, {}, {}],
-  //   },
-  //   {
-  //     id: 5,
-  //     title: "Tierra de osos",
-  //     image:
-  //       "https://play-lh.googleusercontent.com/-7Gl_ipbT697kSJdJb8R_xyEACLSHMjGEXtvIpFSRxaqbXaJiWlu-rJ6VHmRgQaoCKFB=w240-h480-rw",
-  //     rating: 3.5, // Ejemplo de rating
-  //     runTime: 85, // Ejemplo de duración en minutos
-  //     shows: [{}, {}, {}],
-  //   },
-  //   {
-  //     id: 6,
-  //     title: "Toy Story",
-  //     image: "https://es.web.img3.acsta.net/pictures/14/03/17/10/20/509771.jpg",
-  //     rating: 4.8, // Ejemplo de rating
-  //     runTime: 81, // Ejemplo de duración en minutos
-  //     shows: [{}, {}, {}],
-  //   },
-  //   {
-  //     id: 7,
-  //     title: "El señor de los anillos",
-  //     image:
-  //       "https://es.web.img3.acsta.net/medias/nmedia/18/89/67/45/20061512.jpg",
-  //     rating: 4.9, // Ejemplo de rating
-  //     runTime: 178, // Ejemplo de duración en minutos
-  //     shows: [{}, {}, {}],
-  //   },
-  //   {
-  //     id: 8,
-  //     title: "Anabelle",
-  //     image: "https://www.ecartelera.com/carteles/7900/7931/002_m.jpg",
-  //     rating: 3.2, // Ejemplo de rating
-  //     runTime: 109, // Ejemplo de duración en minutos
-  //     shows: [{}, {}, {}],
-  //   },
-  //   {
-  //     id: 9,
-  //     title: "Titanes del Pacífico",
-  //     image:
-  //       "https://i.pinimg.com/736x/17/e1/44/17e144b2e0e49c835fb4092321fefeea.jpg",
-  //     rating: 3.7, // Ejemplo de rating
-  //     runTime: 131, // Ejemplo de duración en minutos
-  //     shows: [{}, {}, {}],
-  //   },
-  //   {
-  //     id: 10,
-  //     title: "John Wick",
-  //     image: "https://es.web.img3.acsta.net/pictures/14/10/01/14/18/135831.jpg",
-  //     rating: 4.5, // Ejemplo de rating
-  //     runTime: 101, // Ejemplo de duración en minutos
-  //     shows: [{}, {}, {}],
-  //   },
-  //   {
-  //     id: 11,
-  //     title: "Cars",
-  //     image: "https://es.web.img2.acsta.net/pictures/14/05/28/11/24/435900.jpg",
-  //     rating: 4.3, // Ejemplo de rating
-  //     runTime: 117, // Ejemplo de duración en minutos
-  //     shows: [{}, {}, {}],
-  //   },
-  // ];
-
-  // const [movies, setMovies] = useState(initialMovies); // estado para manejar películas. (ABM)
 
   const router = createBrowserRouter([
     //Para agregar rutas protegidas necesitamos terminar la cartelera q es una ruta protegida porque el dashboard,
     //el login y register podría acceder cualquiera, y para la cartelera se necesita loguear/registrar.
-    { path: "/", element: <Dashboard /> },
+    { path: "/", 
+      element: <Dashboard /> },
     {
       path: "/login",
       element: <LogIn isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />,
     },
-    { path: "/signin", element: <SignIn /> },
+    { path: "/signin", 
+      element: <SignIn /> },
     {
       path: "/movies",
       // ruta protegida, solo si te logueaste podes acceder. XD
       element: (
         <Protected isLoggedIn={isLoggedIn}>
-          <MoviesDashboard/>
+          <MoviesDashboard movies={movies} />
         </Protected>
       ),
     },
@@ -243,7 +162,8 @@ function App() {
         />
       ),
     },
-    { path: "*", element: <NotFound /> }, // cuando no encuentra ninguna ruta, not found
+    { path: "*", 
+      element: <NotFound /> },
   ]);
 
   return (
